cf4cd7630c3658ccad8c806176a28c4a40f7870a
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2018-06-15 00:13:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+firebase.initializeApp({
+	apiKey: "AIzaSyDOw5sWavkXLQxhosMODln9jmom5gN0AoE",
+	authDomain: "syncrocalendar-f6301.firebaseapp.com",
+	projectId: "syncrocalendar-f6301"
 
+});
+var db = firebase.firestore();
 
+      
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
   } else {
@@ -15,4 +22,34 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function xd(){
     alert('hola');
+};
+
+function RegistrarC(){
+	var NombreC = document.getElementById('NombreC').value;
+	var PasswordC = document.getElementById('PasswordC').value;
+        var cityRef = db.collection('Valores').doc('UID');
+        var UIDC; 
+        var getDoc = cityRef.get()
+    .       then(doc => {
+            if (!doc.exists) {
+            console.log('No such document!');
+            } else {
+            UIDC = (doc.data().Calendar);
+            console.log(UIDC);
+            var Calendar2 = UIDC + 1;
+            var datos = {
+                Nombre: NombreC,
+                Password: PasswordC,
+                UID: UIDC
+            };
+            var dato = {
+              Calendar: Calendar2  
+            };
+        var setDoc = db.collection('CalInf').doc(''+UIDC).set(datos);
+        var setDoc = db.collection('Valores').doc('UID').set(dato);
+       }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });  
 };
